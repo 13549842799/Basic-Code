618c64a48a9ef0a79b7ad5b11a0dd70aa334deea
cyz <caiyangzhi123@163.com>
2020-05-13 17:55:32 +0800

    ti

--- src/main/java/com/cyz/basic/util/HttpUtil.java
+++ src/main/java/com/cyz/basic/util/HttpUtil.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashMap;
@@ -66,6 +67,36 @@ public abstract class HttpUtil {
 	public static boolean isPhoneLogin(HttpServletRequest request) {
 		return checkHttpOrigin(request) == 0;
 	}
+	
+	public static String getIpAddr(HttpServletRequest request) {   
+        String ip = request.getHeader("x-forwarded-for");   
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {   
+            ip = request.getHeader("Proxy-Client-IP");   
+        }   
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {   
+            ip = request.getHeader("WL-Proxy-Client-IP");   
+        }   
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {   
+            ip = request.getRemoteAddr();   
+            if(ip.equals("127.0.0.1")){     
+                //根据网卡取本机配置的IP     
+                InetAddress inet=null;     
+                try {     
+                    inet = InetAddress.getLocalHost();     
+                } catch (Exception e) {     
+                    e.printStackTrace();     
+                }     
+                ip= inet.getHostAddress();     
+            }  
+        }   
+        // 多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割  
+        if(ip != null && ip.length() > 15){    
+            if(ip.indexOf(",")>0){     
+                ip = ip.substring(0,ip.indexOf(","));     
+            }     
+        }     
+        return ip;   
+    }
 	
 	public static Map<String, Object> request(String url, Map<String, String> params, Map<String, String> headers, String type, String responseType) {
 		switch (type) {
